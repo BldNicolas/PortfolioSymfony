3a1fa997b7eac6851e8d8c0ae48bfb046ef4927b
Nicolas <ni.berlaud@gmail.com>
2025-01-08 16:56:48 +0100

    ✨ Show Portfolios in index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PortfolioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(PortfolioRepository $portfolioRepository): Response
     {
         return $this->render('index/index.html.twig', [
+            'portfolios' => $portfolioRepository->findAll(),
         ]);
 
     }
